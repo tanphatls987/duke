f046b441e3697039f500f9fe5ebbc399c643f9db
tanphatls987 <phattt192011@gmail.com>
2020-01-18 15:47:14 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    private static void printLogo() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+    public static void main(String[] args) {
+        printLogo();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String cmd = scanner.nextLine();
+            if (cmd.equals("bye")) {
+                System.out.println("End program.")
+                break;
+            } else {
+                System.out.println(cmd);
+            }
+        }
     }
 }
